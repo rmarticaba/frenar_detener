9fb6b22863abcd8194fc0f95e41a4ac46f405a50
rmarticaba <49385764+rmarticaba@users.noreply.github.com>
2023-05-03 22:13:49 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,36 +1,17 @@
 function ejercicio8 () {
-    avanzar()
-    evitar_colision()
+    distance = maqueen.Ultrasonic(PingUnit.Centimeters)
+    if (distance <= 30 && distance > 20) {
+    	maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+    } else if (distance <= 20 && distance > 10) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+    } else if (distance <= 10) {
+        maqueen.motorStop(maqueen.Motors.All)
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 70)
+    }
     basic.pause(200)
 }
-function evitar_colision () {
-    if (distancia_de_colision()) {
-        maqueen.motorStop(maqueen.Motors.All)
-        speed = 0
-    }
-}
-function distancia_de_frenado () {
-    return maqueen.Ultrasonic(PingUnit.Centimeters) <= 25 && maqueen.Ultrasonic(PingUnit.Centimeters) > 6
-}
-function distancia_de_colision () {
-    return maqueen.Ultrasonic(PingUnit.Centimeters) <= 6
-}
-function reducir_velocidad () {
-    if (speed > vel_minima) {
-        speed = speed - 20
-    }
-}
-function avanzar () {
-    if (distancia_de_frenado()) {
-        reducir_velocidad()
-    }
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, speed)
-}
-let speed = 0
-let vel_minima = 0
-vel_minima = 10
-speed = 90
-maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, speed)
+let distance = 0
 basic.showString("Hola")
 basic.forever(function () {
     ejercicio8()
